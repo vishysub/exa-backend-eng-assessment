993a0774a2578d7840668b03e75ae672136cd6cd
viswanathsubramanian <viswanath.subramanian@blackboard.com>
2023-06-03 15:47:46 +0530

    Script to load data from json to separate Postgres tables based on resource type

--- db/init.sql
+++ db/init.sql
@@ -4,4 +4,5 @@ CREATE TABLE bundle (
     resource jsonb
 );
 
+INSERT INTO bundle VALUES (1,  '{"name": {"fire":"test","age": 23}, "tags": ["Improvements", "Office"], "finished": true}');
 GRANT ALL PRIVILEGES ON DATABASE fhir TO postgres;
--- utils/load_resources.py
+++ utils/load_resources.py
@@ -1,27 +1,38 @@
-
+'''
+Load data to postgres tables;
+'''
 import os
 import json
-import requests
 from sqlalchemy import create_engine
 path_to_json_files = 'data/'
+from psycopg2.extras import Json
 os.chdir('..')
-#
 engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/fhir')
 con = engine.connect()
-#
-# query = """INSERT INTO bundle VALUES (1,  '{"name": {"fire":"test","age": 23}, "tags": ["Improvements", "Office"], "finished": true}');"""
 json_file_names = [filename for filename in os.listdir(path_to_json_files) if filename.endswith('.json')]
-# headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
-# resources = dict()
-# keys = set()
+unique_id = 1
+ser_no = 1
 for json_file_name in json_file_names:
     with open(os.path.join(path_to_json_files, json_file_name)) as json_file:
         data = json.load(json_file)
-        # data = data.replace("true", "\"true\"")
-        # data = data.replace("false", "\"false\"")
-        print(json.dumps(data))
-        # query = """INSERT INTO bundle VALUES (3,'{"name": {"fire":true,"age": 23}, "tags": ["Improvements", "Office"], "finished": true}');"""
-        query = f"INSERT INTO bundle VALUES (2,\'{json.dumps(data)}\');"
-        print(query)
-        x=con.execute(query)
-        break;
+
+        for res_data in data['entry']:
+            resource_type = res_data['resource'].pop('resourceType')
+            create_table = f"""CREATE TABLE IF NOT EXISTS {resource_type} ( \
+                            id SERIAL PRIMARY KEY, \
+                            patientid varchar,
+                            resource jsonb
+                        );"""
+            con.execute(create_table)
+            dt = Json(res_data['resource'])
+            print(dt)
+            query = f"INSERT INTO {resource_type} VALUES ({ser_no},{unique_id},{dt});"
+            try:
+                val=con.execute(query)
+            except Exception as e:
+                print("Unsupported Data--------- Skipping")
+                pass
+            ser_no +=1
+    print(f"end of file{json_file_name}")
+    unique_id +=1
+
