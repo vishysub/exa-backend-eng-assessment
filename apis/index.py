3b614d02db58db0f74bb67b68318d2ad5ea74040
viswanathsubramanian <viswanath.subramanian@blackboard.com>
2023-06-04 20:15:40 +0530

    Added getting details based on patientid logic

--- apis/index.py
+++ apis/index.py
@@ -16,24 +16,26 @@ def details(keys: dict, resource: str, p_stop: bool = False):
 
     input_data = keys.get('keys')
     if 'patientid' in input_data.keys():
-        query = f"select patientid,resource from {resource} where patientid = '{input_data['patientid']}';"
+        query = f"select patientid,resource from {resource} where patientid in {tuple(i[0] for i in input_data['patientid'])};"
     else:
         condition_fields = json.dumps(input_data)
         query = f"select patientid,resource from {resource} where resource @> '{condition_fields}';"
-    pid = 1
+        pid_query = f"select distinct patientid from {resource} where resource @> '{condition_fields}';"
+        pid = con.execute(pid_query).fetchall()
     resp = con.execute(query).fetchall()
-
-    data = resp[0][1]
+    data = resp
+    filtered_data = []
 
 
     if requested_fields.get(resource) is not None:
-        result[resource] = dict(filter(lambda item: item[0] in requested_fields.get(resource), data.items()))
-
+        for i in data:
+            filtered_data.append(dict(filter(lambda item: item[0] in requested_fields.get(resource), i[1].items())))
+        result[resource] = filtered_data
         requested_fields.pop(resource)
 
         if p_stop == False:
             for k,v in requested_fields.items():
-                result.update(details({'keys':{'patientid':pid},'requested_fields': {k: v}},k,True))
+                result.update(details({'keys': {'patientid':pid},'requested_fields': {k: v}},k,True))
 
     else:
         result[resource] = data
