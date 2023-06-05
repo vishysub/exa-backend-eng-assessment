ba159aadfac4add939db8c430f5de600d7f04f9a
viswanathsubramanian <viswanath.subramanian@blackboard.com>
2023-06-05 20:17:03 +0530

    Updates to core logic , docker ; Make db creds as env

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # GETMED - WebApp
+
+
+## Overview
+  * GetMed is an api capable of providing the medical record data (FHIR Format).
+  * The api has been built using Python FastApi implementation on PostgresDB deployed using docker.
+  * load_resources.py script is provided to load the sample data into the PostgreDB.
+  
+
+## Requirements
+
+* `python` Python 3+
+* `pip` 
+* `Docker`
+
+## Running the webapp  (Setup Postgres DB and Initialize WebApp)
+* docker-compose up --build 
+* 
+## Load the sample data
+python utils/load_resources
+
+
+
+#Usage :
+ * Url format :[GET] :"http://{hostname}}:{port}}/{resource}"
+ * GetMed provides api endpoint customized for the needs of the user.The users can choose the type of data that they requires along with the supporting data required.
+
+##Request and Response Details
+ * Ex: To get the claims of a patient given the patient's name.(Resource Defined as per FHIR Standards)
+ * To get the patient data the api url {resource} = patient. ("https://.../patient)
+   1. keys -> Key value pair to be used as a search element . (Provide patient's name field here)
+   2. requested_fields -> Fields that are requried from each resource for all patients who's immunization Status is "completed". 
+   #RESPONSE
+   1. Count -> Count of Patients in the Response
+
+ 
+# Sample Request :
+{
+"keys" : {"id": "b5cb6364-0087-2091-638b-9b23cd827257"},
+"requested_fields" : { "immunization":["id"],
+                        "patient" :["name"],
+                        "claim":["id"]
+                      }
+}
+# Sample Response 
+{
+    "Count": 1
+    "1": {
+        "immunization": [
+            {
+                "id": "b5cb6364-0087-2091-638b-9b23cd827257"
+            }
+        ],
+        "patient": [
+            {
+                "name": [
+                    {
+                        "use": "official",
+                        "given": [
+                            "Everett935"
+                        ],
+                        "family": "Littel644",
+                        "prefix": [
+                            "Mr."
+                        ]
+                    }
+                ]
+            }
+        ],
+        "claim": [
+            {
+                "id": "17ab2629-6969-0b46-f7a1-d8c5346baff2"
+            },
+            {
+                "id": "994c7dcc-b960-af5b-cb55-464d6892e53b"
+            },
+            {"..."},
+            {
+                "id": "0d8b13a8-5576-c890-9a04-c15c2097457e"
+            },
+            {
+                "id": "c1243ecf-0017-7c32-6e22-83d2bb47d029"
+            }
+        ]
+    }
+}
+
+
+You can access PostgreSQL on localhost:5432 and Apis on localhost:8000
--- apis/index.py
+++ apis/index.py
@@ -1,26 +1,22 @@
 """
 The GET Details api
-REQUEST :
-        {"keys" : {"status": "completed"},
-    "requested_fields" : {"patient" :["id","name"],
-                        "immunization": ["id","meta"]
-    }
-    }
-
-
-RESPONSE :
-
 """
-from fastapi import FastAPI
-from sqlalchemy import create_engine
+import os
 import json
-import logging
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from sqlalchemy import create_engine
 
-engine = create_engine('postgresql+psycopg2://postgres:password@db:5432/fhir')
-con = engine.connect()
+load_dotenv()
 app = FastAPI()
 
-logger = logging.getLogger("logging")
+
+def db_connect():
+    engine = create_engine(f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DB")}')
+    connection = engine.connect()
+    return connection
+
 
 def FormatResponse(status_code, message):
     return_res = {"message": message}
@@ -30,14 +26,6 @@ def FormatResponse(status_code, message):
             "errorType": "Bad Request",
 
         },
-        "Unauthorized": {"httpStatus": 401, "errorType": "Unauthorized"},
-        "Forbidden": {"httpStatus": 403, "errorType": "Forbidden"},
-        "NotFound": {
-            "httpStatus": 404,
-            "errorType": "Not Found",
-
-        },
-        "Conflict": {"httpStatus": 409, "errorType": "Conflict"},
         "InternalServerError": {
             "httpStatus": 500,
             "errorType": "Server Error",
@@ -45,7 +33,7 @@ def FormatResponse(status_code, message):
     }
 
     return_res.update(response_context.get(status_code))
-    return return_res
+    return JSONResponse(content=return_res, status_code=return_res['httpStatus'])
 
 
 @app.get("/{resource}")
@@ -53,34 +41,39 @@ def details(keys: dict, resource: str, p_stop: bool = False):
     result = {}
     requested_fields = keys.get('requested_fields')
     input_data = keys.get('keys')
-    if not isinstance(requested_fields,dict):
-        return FormatResponse("BadRequest", "Missing/Improper request_fields field")
-
-    if 'patientid' in input_data.keys():
-        query = f"select patientid,resource from {resource} where patientid in {tuple(i[0] for i in input_data['patientid']) + ('None',)};"
-    else:
-        condition_fields = json.dumps(input_data)
-        query = f"select patientid,resource from {resource} where resource @> '{condition_fields}';"
-        pid_query = f"select distinct patientid from {resource} where resource @> '{condition_fields}';"
-        pid = con.execute(pid_query).fetchall()
-    data = con.execute(query).fetchall()
-    filtered_data = []
-    if requested_fields.get(resource) is not None:
-        for i in data:
-            filter_data = dict(filter(lambda item: item[0] in requested_fields.get(resource), i[1].items()))
-            if result.get(i[0],{}).get(resource) is not None:
-                result[i[0]][resource].append(filter_data)
+    if not (isinstance(requested_fields, dict) and isinstance(input_data, dict)):
+        return FormatResponse("BadRequest", "Missing/Improper fields")
+    try:
+        with db_connect() as con:
+            fields = "patientid,resource"
+            if 'patientid' in input_data.keys():
+                query = f"select {fields} from {resource} where patientid in {tuple(i[0] for i in input_data['patientid']) + ('None',)};"
             else:
-                result[i[0]] = {resource: [filter_data]}
+                condition_fields = json.dumps(input_data)
+                where_condition = f"resource @> '{condition_fields}'"
+                query = f"select {fields} from {resource} where {where_condition};"
+                pid_query = f"select distinct patientid from {resource} where {where_condition};"
+                pid = con.execute(pid_query).fetchall()
+            data = con.execute(query).fetchall()
+        if requested_fields.get(resource) is not None:
+            for i in data:
+                filter_data = dict(filter(lambda item: item[0] in requested_fields.get(resource), i[1].items()))
+                if result.get(i[0], {}).get(resource) is not None:
+                    result[i[0]][resource].append(filter_data)
+                else:
+                    result[i[0]] = {resource: [filter_data]}
 
-        requested_fields.pop(resource)
+            requested_fields.pop(resource)
 
-        if p_stop == False:
-            for res, fields in requested_fields.items():
-                response = details({'keys': {'patientid': pid}, 'requested_fields': {res: fields}}, res, True)
-                for i in pid:
-                    result[i[0]].update(response[i[0]])
+            if p_stop == False:
+                for res, fields in requested_fields.items():
+                    response = details({'keys': {'patientid': pid}, 'requested_fields': {res: fields}}, res, True)
+                    for i in pid:
+                        result[i[0]].update(response[i[0]])
 
-    else:
-        result[resource] = data
-    return result
+        else:
+            result[resource] = data
+        result['Count'] = len(result.keys())
+        return result
+    except Exception as e:
+        return FormatResponse("InternalServerError", str(e)[:50])
